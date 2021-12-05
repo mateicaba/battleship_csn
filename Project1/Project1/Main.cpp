48213a6cb03247dd1739f9ea528d77e3249ef09f
ASavastru <89639595+ASavastru@users.noreply.github.com>
2021-12-06 01:47:51 +0200

    Added Text Box
    press Return to select textbox
    press Escape to deselect textbox

--- Project1/Project1/Main.cpp
+++ Project1/Project1/Main.cpp
@@ -1,22 +1,49 @@
-
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Not Tetris");
+#include "Textbox.h"
 
-	sf::Event event;
+int main() {
+	sf::RenderWindow window;
 
+	sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2) - 445, (sf::VideoMode::getDesktopMode().height / 2) - 480);
+
+	window.create(sf::VideoMode(900, 900), "Battleship", sf::Style::Titlebar | sf::Style::Close);
+	window.setPosition(centerWindow);
+
+	window.setKeyRepeatEnabled(true);
+
+	sf::Font arial;
+	arial.loadFromFile("arial.ttf");
+	Textbox textbox1(15, sf::Color::White, false);
+	textbox1.setFont(arial);
+	textbox1.setPosition({ 100,100 });
+	textbox1.setLimit(true, 10);
+
+	//Main loop
 	while (window.isOpen()) {
+		sf::Event Event;
 
-		while (window.pollEvent(event)) {
-
-			if (event.type == sf::Event::Closed) {
-
-				window.close();
-			}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+			textbox1.setSelected(true);
 		}
-	}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+			textbox1.setSelected(false);
+		}
 
-	return 0;
+		//Event loop
+		while (window.pollEvent(Event)) {
+			switch (Event.type) {
+
+			case sf::Event::Closed:
+				window.close();
+			case sf::Event::TextEntered:
+				textbox1.typedOn(Event);
+			}
+		
+		}
+		window.clear();
+		textbox1.drawTo(window);
+		window.display();
+	}
 }
